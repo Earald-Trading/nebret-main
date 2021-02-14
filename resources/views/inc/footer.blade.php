ddd03f2fb2c598cebe2c711b8018846f90d9f2c5
Natnael Tilaye <natnaeltilaye30@gmail.com>
2021-02-14 11:08:59 +0300

    added stying to the landing page, nav-bar and footer.

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,93 @@
+<footer class="footer navbar-light bg-white shadow-sm">
+    <div class="container">
+        <div class="main-footer widgets-dark typo-light my-5 py-5">
+            <div class="container">
+                <div class="row">
+
+                    <!--About AFMA-->
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+                        <div class="widget subscribe no-box">
+                            <h5 class="widget-title font-weight-bold my-0 py-0">Alatinos Ethiopian Filmmakers' Association<span></span></h5>
+                            <hr class="footer-horizontal-break mt-0 pt-0 ml-0">
+                            <p>About the company, little discription will goes here.. </p>
+                        </div>
+                    </div>
+
+                    <!--Location-->
+                    <div class="col-xs-12 col-sm-6 col-md-3 mb-0 mt-3">
+                        <h5 class="widget-title my-0 py-0">Location<span></span></h5>
+                        <hr class="footer-horizontal-break mt-0 pt-0 w-50 ml-0">
+                        <div class="card card-cascade narrower">
+                            <div class="card-body card-body-cascade text-center mx-0 my-0 px-0 py-0 mb-0 pb-0">
+
+                                <!--Google Maps-->
+                                <div div id="map-container-google-9" class="z-depth-1-half map-container-5" style="height: 200px">
+
+                                    <!--Embedded Map Tag-->
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.344953741935!2d38.747410214276556!3d9.032263391426033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85f721e32cdd%3A0xa97917b43251d08a!2sRussian%20Center%20for%20Science%20and%20Culture!5e0!3m2!1sen!2set!4v1584728767976!5m2!1sen!2set" width="400" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--Affiliate Links-->
+                    <div class="col-xs-12 col-sm-6 col-md-3 mt-3">
+                        <div class="widget no-box">
+                            <h5 class="widget-title my-0 py-0">Affiliate Links<span></span></h5>
+                            <hr class="footer-horizontal-break mt-0 pt-0 w-50 ml-0">
+                            <ul class="thumbnail-widget list-unstyled">
+                                <li>
+                                    <div class="thumb-content"><a href="#.">Get Started</a></div>
+                                </li>
+                                <li>
+                                    <div class="thumb-content"><a href="#.">Top Leaders</a></div>
+                                </li>
+                                <li>
+                                    <div class="thumb-content"><a href="#.">Success Stories</a></div>
+                                </li>
+                                <li>
+                                    <div class="thumb-content"><a href="#.">Event/Tickets</a></div>
+                                </li>
+                                <li>
+                                    <div class="thumb-content"><a href="#.">News</a></div>
+                                </li>
+                                <li>
+                                    <div class="thumb-content"><a href="#.">Lifestyle</a></div>
+                                </li>
+                                <li>
+                                    <div class="thumb-content"><a href="#.">About</a></div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!--Social-media Handles-->
+                    <div class="col-xs-12 col-sm-6 col-md-3 mt-3">
+                        <div class="widget no-box">
+                            <h5 class="widget-title my-0 py-0">Connect with Us<span></span></h5>
+                            <hr class="footer-horizontal-break mt-0 pt-0 w-75 ml-0">
+                            <p><a href="mailto:info@domain.com" title="glorythemes">info@domain.com</a></p>
+                            <ul class="social-footer list-inline ml-3">
+                                <li class="list-inline-item"><a href="https://www.facebook.com/" target="_blank" title="Facebook" class="fa fa-facebook button button-primary"></a></li>
+                                <li class="list-inline-item"><a href="https://twitter.com" target="_blank" title="Twitter" class="fa fa-twitter button button-primary"></a></li>
+                                <li class="list-inline-item"><a href="https://www.instagram.com/" target="_blank" title="Instagram" class="fa fa-instagram button button-primary"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Copyright Statement-->
+        <div class="footer-copyright">
+            <div class="container">
+                <div class="row">
+                    <hr class="footer-horizontal-break w-75 text-center">
+                    <div class="col-md-12 text-center mb-0 pb-0">
+                        <p class="text-small">Copyright. <i class="font-weight-bold">Alatinos Ethiopian Filmmakers' Association</i> © 2020. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -77,42 +77,42 @@
                             <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 1"
                                 aria-label="Listing Image 1" aria-describedby="images1" name="images[]">
                             <div class="input-group-append">
-                                <button class="btn btn-outline-primary" id="images1" type="button">Choose File</button>
+                                <span class="btn btn-outline-primary" id="images1">Choose File</span>
                             </div>
                         </div>
                         <div class="input-group my-2 row">
                             <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 1"
                                 aria-label="Listing Image 2" aria-describedby="images2" name="images[]">
                             <div class="input-group-append">
-                                <button class="btn btn-outline-primary" id="images2" type="button">Choose File</button>
+                                <span class="btn btn-outline-primary" id="images2">Choose File</span>
                             </div>
                         </div>
                         <div class="input-group my-2 row">
                             <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 3"
                                 aria-label="Listing Image 3" aria-describedby="images3" name="images[]">
                             <div class="input-group-append">
-                                <button class="btn btn-outline-primary" id="images3" type="button">Choose File</button>
+                                <span class="btn btn-outline-primary" id="images3">Choose File</span>
                             </div>
                         </div>
                         <div class="input-group my-2 row">
                             <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 4"
                                 aria-label="Listing Image 4" aria-describedby="images4" name="images[]">
                             <div class="input-group-append">
-                                <button class="btn btn-outline-primary" id="images4" type="button">Choose File</button>
+                                <span class="btn btn-outline-primary" id="images4">Choose File</span>
                             </div>
                         </div>
                         <div class="input-group my-2 row">
                             <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 5"
                                 aria-label="Listing Image 5" aria-describedby="images5" name="images[]">
                             <div class="input-group-append">
-                                <button class="btn btn-outline-primary" id="images5" type="button">Choose File</button>
+                                <span class="btn btn-outline-primary" id="images5">Choose File</span>
                             </div>
                         </div>
                         <div class="input-group my-2 row">
                             <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 6"
                                 aria-label="Listing Image 6" aria-describedby="images6" name="images[]">
                             <div class="input-group-append">
-                                <button class="btn btn-outline-primary" id="images6" type="button">Choose File</button>
+                                <span class="btn btn-outline-primary" id="images6">Choose File</span>
                             </div>
                         </div>
                     </div>
@@ -122,105 +122,4 @@
             </form>
         </div>
     </div>
-        {{-- <div class="row full-width">
-            <form method="POST" action="{{ route('upload.store') }}" enctype="multipart/form-data">
-                    <div class="my-3 col">
-                        <label class="form-label">Email</label>
-                        <input class="form-control" type="email" name="user_email" id="id_email" required>
-                    </div>
-
-                    <div class="my-3 col">
-                        <label class="form-label">Price</label>
-                        <input class="form-control" type="number" step=0.01 name="price" id="id_price" required>
-                    </div>
-
-                    <div class="my-3 col">
-                        <label class="form-label">Description</label>
-                        <textarea name="logline" class="form-control" id="id_logline" required></textarea>
-                    </div>
-
-                    <div class="my-3 col">
-                        <label class="form-label">Youtube Video id</label>
-                        <input class="form-control" name="youtube_id" id="id_youtube" required>
-                    </div>
-
-                    <hr>
-
-                    <div class="text text-secondary text-left lead text-capitalize text-uppercase mt-4 mb-3">Location Information</div>
-                    <div class="my-3 col">
-                        <label class="form-label">Latitude</label>
-                        <input class="form-control" type="number" step=any name="latitude" id="id_latitude">
-                    </div>
-                    <div class="my-3 col">
-                        <label class="form-label">Longitude</label>
-                        <input class="form-control" type="number" step=any name="longitude" id="id_longtiude">
-                    </div>
-                    <div class="my-3 col">
-                        <label class="form-label">Sub City</label>
-                        <input class="form-control" name="subcity" id="id_subcity">
-                    </div>
-                    <div class="my-3 col">
-                        <label class="form-label">Wereda</label>
-                        <input class="form-control" type="number" name="wereda" id="id_wereda">
-                    </div>
-                    <div class="my-3 col">
-                        <label class="form-label">House Number</label>
-                        <input class="form-control" name="houseno" id="id_houseno">
-                    </div>
-                    <div class="form-check form-switch my-3 col">
-                        <input class="form-check-input" type="checkbox" name="featured" id="id_featured">
-                        <label class="form-label">Featured</label>
-                    </div>
-                    <div class="w-100"></div>
-                    <div>
-                        <label class="form-label text text-secondary text-left lead text-capitalize text-uppercase">Images</label>
-                    </div>
-                    <div class="input-group mt-3 mb-2 col">
-                        <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 1"
-                            aria-label="Listing Image 1" aria-describedby="images1" name="images[]">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-primary" id="images1" type="button">Choose File</button>
-                        </div>
-                    </div>
-                    <div class="input-group my-2 col">
-                        <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 1"
-                            aria-label="Listing Image 2" aria-describedby="images2" name="images[]">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-primary" id="images2" type="button">Choose File</button>
-                        </div>
-                    </div>
-                    <div class="input-group my-2 col">
-                        <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 3"
-                            aria-label="Listing Image 3" aria-describedby="images3" name="images[]">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-primary" id="images3" type="button">Choose File</button>
-                        </div>
-                    </div>
-                    <div class="input-group my-2 col">
-                        <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 4"
-                            aria-label="Listing Image 4" aria-describedby="images4" name="images[]">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-primary" id="images4" type="button">Choose File</button>
-                        </div>
-                    </div>
-                    <div class="input-group my-2 col">
-                        <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 5"
-                            aria-label="Listing Image 5" aria-describedby="images5" name="images[]">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-primary" id="images5" type="button">Choose File</button>
-                        </div>
-                    </div>
-                    <div class="input-group my-2 col">
-                        <input type="file" class="custom-file-input mt-2 form-control" placeholder="Listing Image 6"
-                            aria-label="Listing Image 6" aria-describedby="images6" name="images[]">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-primary" id="images6" type="button">Choose File</button>
-                        </div>
-                    </div>
-                </div>
-                <input name="_token" type="hidden" value="{{ csrf_token() }}" />
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    </div> --}}
 @endsection
