4f4f8a40bfa90ad028703a2976db96ffa6f84aa2
Natnael Tilaye <natnaeltilaye30@gmail.com>
2021-03-05 12:43:02 +0300

    added dictionary for AMH

---
+++ resources/lang/en/body.php
@@ -0,0 +1,114 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Body Content Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the application to build
+    | all the views. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'listing' => 'Listing',
+
+    // House Types
+    'appartment' => 'Appartment',
+    'g+__building' => 'G+__Building',
+    'land' => 'Land',
+    'villa' => 'Villa',
+
+    //Measurements
+    'area' => 'Area',
+    'sqmr' => 'sqmr',
+    'bedroom' => 'Bedroom',
+    'bathroom' => 'Bathroom',
+
+    //Listing Types
+    'featured' => 'Featured',
+    'reduced_price' => 'Reduced Price',
+    'new_construction' => 'New Construction',
+    'open_house' => 'Open House',
+    'for_rent' => 'For Rent',
+    'for_sale' => 'For Sale',
+    'foreclosure' => 'Foreclosure',
+    'joint_venture' => 'Joint Venture',
+
+    //Keywords
+    'description' => 'Description',
+    'video_footage' => 'Video Footage',
+    'location' => 'Location',
+    'site_map' => 'Site Map',
+    'connect_with_us' => 'Connect With Us',
+    'property_type' => 'Property Type',
+    'year_built' => 'Year Built',
+    'price_per_sqmr' => 'Price per sqmr',
+    'status' => 'Status',
+    'etb' => 'ETB',
+    'name' => 'Fullname',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'email' => 'Email',
+    'password' => 'Password',
+    'phone_number' => 'Phone Number',
+    'nav' => [
+        'home' => 'HOME',
+        'available_listing' => 'AVAILABLE LISTINGS',
+        'sell_rent' => 'SELL/RENT',
+        'upload' => 'UPLOAD',
+        'users' => 'USERS',
+        'about_us' => 'About Us',
+        'login' => 'Login',
+        'register' => 'Register',
+    ],
+    'buttons' => [
+        'login_to_see_comparative_analysis' => 'Login to See Comparative Analysis',
+        'contact_agent_for_more' => 'Contact Agent for More',
+        'login' => 'Login',
+        'register' => 'Register',
+        'submit' => 'Submit',
+        'upload' => 'Upload',
+    ],
+    'places' => [
+        'area' => 'Area',
+        'ethiopia' => 'Ethiopia',
+        'addis_ababa' => 'Addis Ababa',
+        'addis_ketema' => 'Addis Ketema',
+        'akaky_kaliti' => 'Akaky Kaliti',
+        'arada' => 'Arada',
+        'bole' => 'Bole',
+        'gullele' => 'Gullele',
+        'kirkos' => 'Kirkos',
+        'kolfe_keranio' => 'Kolfe Keranio',
+        'lemi_kura' => 'Lemi Kura',
+        'lideta' => 'Lideta',
+        'nifas_silk_lafto' => 'Nifas-silk Lafto',
+        'yeka' => 'Yeka',
+    ],
+    'badges' => [
+        'featured' => 'Featured',
+        'upload_time' => 'Uploaded :time ago',
+        'status' => 'Status',
+    ],
+    'about_us' => [
+        'who_we_are' => 'Who Are We?',
+        'who_we_are_disc' => 'Nebret Property Management is a Asset management company founded by young ambitious entrepreneurs, Yonatan Amha and Natnael Girma.',
+        'what_we_do' => 'What Do We Do?',
+        'what_we_do_disc' => 'They created the company because they wanted to creat a solution for seller, new homeowners and other buyers that are frustrated with the system in place.',
+        'reliable' => 'Reliable',
+        'reliable_disc' => 'Nebret PM is here to solve the struggle of buying new properties and create a simple and enjoyable experience to our clients.',
+        'excellence' => 'Excellence',
+        'excellence_disc' => 'Our company is experienced to help customers buy, sell, rent and manage assets such as commercial buildings, apartments and villas.',
+        'professional' => 'Professional',
+        'professional_disc_one' => 'Nebret PM connects you to the right clients at the right time with the right price.',
+        'professional_disc_two' => 'We work with buyers and sellers across the world who are interested to get involved in Ethiopia’s real estate market and those who want to land their next home.',
+        'agent_card_title' => 'Are You an Agent?',
+        'agent_card_tagline' => 'We would love to work with you if you are intrested.',
+        'agent_card_subtitle' => 'Leave us your details below.',
+
+    ]
+
+];
