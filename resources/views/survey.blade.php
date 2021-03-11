7ee71a936fa1db32912edd6e680d8a8b58c45513
Abel Mulugeta <abelmulugeta2014@gmail.com>
2021-03-11 14:03:13 +0300

    update survey

--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -4,8 +4,8 @@
     <div class="container my-5">
         <div class="card py-3" style="width: 70% !important; margin: auto !important;">
             <iframe
-                src="https://docs.google.com/forms/d/e/1FAIpQLSdxSbhcsI6Dgwah3D_Myi_gG5Fq258otvDtf0guFUSpKL-1CQ/viewform?embedded=true"
-                width="700" height="780" frameborder="0" marginheight="0" marginwidth="0"
+                src="https://docs.google.com/forms/d/e/1FAIpQLSdW5E1u80VRKVvfp3kVq1Y2UGBaOYB8G0Z_lsDR5BMMTB6bzg/viewform?embedded=true&entry"
+                width="700" height="1400" frameborder="0" marginheight="0" marginwidth="0"
                 style="width: 97% !important; margin: auto !important;">Loading…</iframe>
         </div>
     </div>
